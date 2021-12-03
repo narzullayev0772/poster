ee3250ff8bef1043f65b13b6e1d39557d4de0740
Developer0772 <alishernarzullayev7777@gmail.com>
2021-12-03 22:44:21 +0500

    push

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// mysql db connection
+const mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "maqola",
+});
+con.connect((err) => {
+  if (err) throw err;
+  console.log("connected");
+});
+
+function insert(username, text) {
+  con.query(
+    `insert into users(username,text) values('${username}','${text}')`,
+    function (err, result) {
+      if (err) throw err;
+      console.log("inserted post");
+    }
+  );
+}
+
+var author,
+  text;
+app.post("/posts", async (req, res, next) => {
+  author = req.body.author;
+  text = req.body.text;
+  insert(author, text);
+});
+var resultArray;
+app.get("/allpost", (req, res) => {
+  con.query("SELECT * FROM users", (err, rows) => {
+    if (err) throw err;
+    res.send(rows)
+  });
+});
+
+
+app.post('/delete',async (req,res,next)=>{
+    if(req.body.id){
+    var sql = `DELETE FROM users WHERE id=${req.body.id}`;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + req.body.id);
+  })}
+  // console.log(req.body.id);
+})
+
+app.get('/posts',(req,res)=>{
+  res.sendFile(__dirname+'/index.html')
+})
+
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("connect");
+});
